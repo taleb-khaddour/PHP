5e8284f0d0eb96a247753e4267a7d7b4885ba6e6
taleb khaddour <taleb.khaddour.20311@gmail.com>
2023-02-20 00:06:36 +0200

    step5 basic2

---
+++ step-5.php
@@ -0,0 +1,32 @@
+<?php 
+
+$n="four";
+switch ($n) {
+    case "zero":
+      echo "0";
+      break;
+    case "one":
+        echo "1";
+      break;
+    case "two":
+        echo "2";
+      break;
+    
+      case "three":
+        echo "3";
+      break;
+
+      case "four":
+        echo "4";
+      break;
+
+      case "five":
+        echo "5";
+      break;
+    default:
+    echo "errore";
+  }
+
+
+
+?>
