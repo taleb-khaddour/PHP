d7ae301bc0a46b4e31f83cef3d445d9ed855f0d7
taleb khaddour <taleb.khaddour.20311@gmail.com>
2023-02-19 19:05:39 +0200

    step8

---
+++ step-8.php
@@ -0,0 +1,14 @@
+<?php
+
+$items = array(
+    'eggs' => array('balade', 'mazere3'),
+    'milk' => array('Fresh', 'Taanayel'),
+    'water-pack' => array('Tanoureen', 'Reem')
+);
+
+$eggs = $items['eggs'][0];
+$water_pack = $items['water-pack'][1];
+
+echo "Hey Sir, Please I need 1 pack of $eggs eggs and 3 $water_pack Water Pack.";
+
+?>
