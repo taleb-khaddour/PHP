07febc80e05ccf94f347af78ed4271033177ebd3
taleb khaddour <taleb.khaddour.20311@gmail.com>
2023-02-19 17:04:01 +0200

    step4

---
+++ step-4.php
@@ -0,0 +1,10 @@
+<?php
+$name = "Gaby";
+$position = "tech consultant";
+$github_url = "https://github.com/gk-git";
+
+echo "Hello, I'm ".$name.", I'm a ".$position." please check my github link ".$github_url.".\n";
+
+echo "Hello, I'm $name, I'm a $position please check my github link $github_url.\n";
+echo 'Hello, I\'m '.$name.', I\'m a '.$position.' please check my github link '.$github_url.".\n";
+?>
