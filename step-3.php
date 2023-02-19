4e7009b50a58904ef4b14f296a4c4157517810e5
taleb khaddour <taleb.khaddour.20311@gmail.com>
2023-02-19 16:59:50 +0200

    step3

---
+++ step-3.php
@@ -0,0 +1,11 @@
+<?php 
+$name="taleb";
+$email="taleb.khaddour@gmail.com";
+echo "My name is " . $name ."\n and my email is " . $email;
+$height=174;
+$width=38;
+$area = $height * $width;
+echo " \n area is ".$area ."\n";
+
+
+?>
